9cbd29caab59fad239214fd8e345fa1374c77ee4
ruizrodr <julian.ruizrodriguez@epfl.ch>
2024-09-12 15:42:23 +0200

    hello world and cmake file

---
+++ build/CMakeFiles/my_executable.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_executable.dir/Hello_world.cc.o"
+  "CMakeFiles/my_executable.dir/Hello_world.cc.o.d"
+  "my_executable"
+  "my_executable.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/my_executable.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
